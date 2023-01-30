cacd923d3746b142a913c372bb7fc4db2f94ea7d
Krutonium <PFCKrutonium@gmail.com>
2023-01-30 17:51:34 -0500

    More debug stuff

--- MacBookFan/getCPUTemp.cs
+++ MacBookFan/getCPUTemp.cs
@@ -6,9 +6,10 @@ public class getCPUTemp
     {
         //Read all temperatures
         List<int> Temperatures = new List<int>();
-        string[] files = Directory.GetFiles("/sys/devices/platform/coretemp.0/hwmon/", "*_input", SearchOption.AllDirectories);
+        string[] files = Directory.GetFiles("/sys/devices/platform/coretemp.0/hwmon/", "*input", SearchOption.AllDirectories);
         foreach (var file in files)
         {
+            Console.WriteLine(file);
             int temp = Int32.Parse(File.ReadAllText(file)) / 1000;
             Temperatures.Add(temp);
         }
