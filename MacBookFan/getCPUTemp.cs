506f48f60cc465b0bf8478fa8e7f6988710763ad
Krutonium <PFCKrutonium@gmail.com>
2023-01-30 18:16:36 -0500

    ReWrite Temp Logic

--- MacBookFan/getCPUTemp.cs
+++ MacBookFan/getCPUTemp.cs
@@ -6,7 +6,23 @@ public class getCPUTemp
     {
         //Read all temperatures
         List<int> Temperatures = new List<int>();
-        string[] files = Directory.GetFiles("/sys/devices/platform/coretemp.0/hwmon/", "*input", SearchOption.AllDirectories);
+        //string[] files = Directory.GetFiles("/sys/devices/platform/coretemp.0/hwmon/", "*input", SearchOption.AllDirectories);
+        //Never completes
+
+        List<string> files = new List<string>(); 
+        List<string> Directories = new List<string>();
+        Directories = Directory.GetDirectories("/sys/devices/platform/coretemp.0/hwmon/").ToList();
+        foreach (var d in Directories)
+        {
+            foreach (var f in Directory.GetFiles(d))
+            {
+                if (f.EndsWith("_input"))
+                {
+                    files.Add(f);
+                }
+            }
+        }
+        
         foreach (var file in files)
         {
             Console.WriteLine(file);
